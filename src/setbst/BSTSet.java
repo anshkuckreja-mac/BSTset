6bb2c2beae5ef85f59698a5383eb2002adb4d8bc
michaelknapman <70975021+michaelknapman@users.noreply.github.com>
2021-03-10 09:30:34 -0500

    Bruh moment....

--- src/setbst/BSTSet.java
+++ src/setbst/BSTSet.java
@@ -24,6 +24,8 @@ public class BSTSet {
 			//use insertion sort to sort, remove duplicates, shorten array.
 			int[] sorted = sort(input);
 			//build bstset
+			//note that this method, because it starts at the
+			//middle, it will be balanced always. its simple maths!
 			root = sortArrToBSTRecursive(sorted, 0, sorted.length-1);
 		}
 	}
@@ -49,39 +51,41 @@ public class BSTSet {
 		return nodes;
 	}
 	
-	//insertion sort from previous lab
-	private int[] sort(int arr[]) {
+	//insertion sort from previous lab AND geeksforgeeks
+	public int[] sort(int arr[]) {
+		//below is the basic insertion sort stuff
 		int arrLength = arr.length;
-
-		for (int i=1; i<arrLength; ++i) {
+		for(int i=1; i<arrLength; ++i) {
 			int currentInt = arr[i];
 			int j = i-1;
-
-			while (j>=0 && arr[j] > currentInt) {
+			//move elements greater than currentInt one position ahead
+			while(j>=0 && arr[j] > currentInt) {
 				arr[j+1] = arr[j];
 				j--;
 			}
 			arr[j+1] = currentInt;
 		}
+		//remove duplicates too!
 		int hold = arr[arrLength-1];
 		int counter = 0;
-		for (int i=0; i<arrLength; i++) {
-			if (arr[i] != hold) {
+		for(int i=0; i<arrLength; i++) {
+			if(arr[i] != hold) {
 				counter++;
 			}
 			hold = arr[i];
 		}
-		int[] output = new int[counter];
+		int[] output = new int[counter]; //# of uniques
 		hold = arr[arrLength-1];
 		counter = 0;
-		for (int i=0; i<arrLength; i++) {
-			if (arr[i] != hold) {
+		for(int i=0; i<arrLength; i++) {
+			if(arr[i] != hold) {
 				output[counter] = arr[i];
 				counter++;
 			}
 			hold = arr[i];
 		}
 		return output;
+		//time complexity is n^2 but i dont think we need to know that
 	}
 
 	
@@ -90,47 +94,299 @@ public class BSTSet {
 	public boolean isIn(int v) {
 		//return true if integer v is an element of this BSTSet
 		
-		//else return false
-		return false;
+		//base case "else return false"
+		if (root == null) {
+			return false;
+		}
+		
+		//call a recursive function that literally goes through all nodes
+		//and checks if the element exists or not.
+		return (isInRecursive(root, v));
+		
+		//log n because we do binary search.
+	}
+
+	
+	//helper method for isIn
+	private boolean isInRecursive(TNode node, int v) {
+		
+		//if node doesnt exist
+		if(node == null) {
+			return false;
+		}
+
+		//node element matches
+		if(node.element == v) {
+			return true;
+		}
+
+		//check and return reuslt from left tree child
+		boolean leftCheck = isInRecursive(node.left, v);
+
+		if (leftCheck == true) {
+			return true;
+		}
+		//if its false then try right side.
+
+		//check and return reuslt from right tree child
+		boolean rightCheck = isInRecursive(node.right, v);
+
+		return rightCheck; //can be false or true.
+		
 	}
 	
 	public void add(int v) {
 		//adds v to this bstset if v isnt a repitition.
 		
-		//else doesnt do anything.
+		//trivial case where root is null, just make the root.
+		if(root == null) {
+			root = new TNode(v, null, null);
+		} 
+		else {
+			//call a recursive add function that checks each child tree
+			//it keeps on going until it finds where it can append
+			//or until it finds its repetition
+			addRecursive(root,v);
+		}
+		//if repetition, nothing happens.
+		
+		//log n because we do binary search and operations
 	}
+	
+	//helper function for add
+	public TNode addRecursive(TNode node, int v) {
+
+		//base case if empty tree
+		if(node == null) {
+			node = new TNode(v,null,null);
+		}
+		
+		//add to left tree if value is less than node
+		if(v<node.element) {
+			node.left = addRecursive(node.left,v);
+		}
+		//add to right tree if value is less than node
+		else if(v>node.element) {
+			node.right = addRecursive(node.right,v);
+		}
+		
+		//if everything fails just return node
+		return node;
+	}
+	
 	public boolean remove(int v) {
 		//removes v if present, and returns true
+
+		//if v isnt in the tree then dont do anything
+		if(isIn(v) == false) {
+			return false;
+		}
+		//else then remove and return true
+		else {
+			removeRecursive(root,v);
+			return true;
+		}
 		
-		//else return false
-		return false;
+		//log n because we do binary search and operations
 	}
+	
+	//helper function for remove
+	public TNode removeRecursive(TNode node, int v) {
+		
+		//base case if empty
+		if(node == null) {
+			return node;
+		}
+		
+		//basically to remove the node, we just gotta do
+		//if value is less than node.element then go left
+		//if value is greater than node.element then go right
+		//if its equal, then remove!
+		if(v<node.element) {
+			node.left = removeRecursive(node.left,v);
+		}
+		else if(v>node.element) {
+			node.right = removeRecursive(node.right,v);
+		}
+		else {
+			//this gets weird because we have to 
+			//handle if this is an internal node
+			
+			//if left address is null, just replace the node with the right child!
+			if(node.left == null) {
+				return node.right;
+			}
+			//if right address is null, just replace the node with the left child!
+			else if(node.right == null) {
+				return node.left;
+			}
+			
+			//if the node has two children we need to replace it with the 
+			//smallest value in the right child tree
+			//or the largest value in the left child tree.
+			//how does the first option sound?
+			//anyways call a helper function to find minimum 
+			node.element = minimum(node.right);
+			
+			
+			//now if we call removeRecursive with the new node.element,
+			//node.element = v
+			//so it'll just return the node. this is wack, yo!
+			node.right = removeRecursive(node.right, node.element);
+		}
+		
+		//if the element isnt here, then just return.
+		return node;
+	}
+	
+	//helper function for remove
+	int minimum(TNode node) {
+		int min = node.element;
+		while(node.left != null) {
+			min = node.left.element;
+			root = node.left;
+		}
+		//when you run out just return min
+		return min;
+	}
+	
 	public BSTSet union(BSTSet s) {
 		//return the union of this and s. should not modify the input sets
 		
-		return s; //placeholder
+		//make an empty unioned BSTSet 
+		BSTSet unioned = new BSTSet();
+		
+		//literally copy this into unioned with add
+		unionRecursive(unioned,root);
+		//literally also copy s into unioned too
+		unionRecursive(unioned,s.root);
+
+		return unioned; 
+		
+		//n+m time complexity because we view all nodes in both trees
 	}
+	public void unionRecursive(BSTSet s, TNode node) {
+		
+		if(node != null) {
+			//add elements together
+			s.add(node.element);
+			//try it for left subtree
+			unionRecursive(s,node.left);
+			//try it for right subtree
+			unionRecursive(s,node.right);
+		}
+		
+		else {
+			; //literally do nothing
+		}
+	}
+	
 	public BSTSet intersection(BSTSet s) {
-		//return the intersection of this and s. should not modify the inptu sets
+		//return the intersection of this and s. should not modify the input sets
+		//only the common elements
+		
+		//TODO: do it
 		
 		return s; //placeholder
 	}
+	
+	
+	
 	public BSTSet difference(BSTSet s) {
-		//return the intersection of this and s. should not modify the inptu sets
+		//return the intersection of this and s. should not modify the input sets
+		//basically whats in this thats not in s
 		
-		return s; //placeholder
+		//make an empty intersected BSTSet 
+		BSTSet differed = new BSTSet();
+
+		//then do call differedRecursive to find elements
+		//that are in this and not s (root is this)
+		differenceRecursive(s,root,differed);
+		
+		return differed;
+		
+		//n+m complexity, goes through n and m
+		
+		
+		//Grrrrr i had a recursive remove method...
+		//where you just call remove() from differed when differed contains this
+		//but it didnt work.........
+		//whatever this is simpler
+	}
+	public void differenceRecursive(BSTSet s, TNode node, BSTSet differed) {
+		//if node exists then...
+		if(node != null) {
+			//find if the node's element is in s
+			//if it is, do nothing.
+			//if it isnt then add the element since its part of the difference
+			if(s.isIn(node.element)) {
+				; //will be subtracted by s
+			}
+			else {
+				differed.add(node.element);
+			}
+			//now do left subtree
+			differenceRecursive(s, node.left, differed);
+			//now do right subtree
+			differenceRecursive(s, node.right, differed);
+		}
 	}
 	
 	public int size() {
 		//returns number of elements in this
 		
-		return 0; //placeholder
+		//call a recursive function to find size of whole tree from root
+		return sizeRecursive(root);
+		
+		//n because we need to check each node
 	}
+	public int sizeRecursive(TNode node) {
+		//empty tree base case
+		if(node == null) {
+			return 0;
+		}
+		else {
+			//literally add together and return the sum of the size.
+			//at the end u cant recurse anymore and you just sum.
+			return(sizeRecursive(node.left) + 1 + sizeRecursive(node.right));
+		}
+	}
+	
+	
 	public int height() {
 		//returns height of this
 		
-		return 0; //placeholder
+		//call a recursive function.
+		return heightRecursive(root);
+		
+		//n because we check every node
 	}
+	
+	public int heightRecursive(TNode node) {
+		//eg if just 1 node, height will be zero because
+		//you have it having left height adding 1
+		//but then base case subtracting 1
+		//so its 0
+		
+		//if its empty tree, no height exists. must be -1
+		if(node == null) {
+			return -1;
+		}
+		else {
+			//compute depth of each subtree
+			int leftHeight = heightRecursive(node.left);
+			int rightHeight = heightRecursive(node.right);
+			
+			//use larger one. recursively forever. until you find height finally.
+			if(rightHeight > leftHeight) {
+				return(rightHeight+1);
+			}
+			else {
+				return(leftHeight+1);
+			}
+		}
+	}
+	
 	public void printBSTSet() {
 		if(root==null)
 			System.out.println("The set is empty");
@@ -150,9 +406,13 @@ public class BSTSet {
 	public void printNonRec() {
 		//prints the integers in this in increasing order. nonrecursive.
 		//uses a stack to implement inorder traversal, MyStack used.
+		
+		//TODO: do it!
 	}
 	public void printLevelOrder() {
 		//prints integers in this in level order using a queue, MyQueue
+		
+		//TODO: do it
 	}
 
 	public TNode getRoot() {
@@ -162,11 +422,11 @@ public class BSTSet {
 	public static void main(String[] args) {
 
 		
-		//TODO: asymptotic run time and space complexity must be presented.
+		//TODO: big theta runnng time only
 		//TODO: the tutorial has code for queue
 		//should probably attempt bonus cuz i think i have a balanced one.
 		//geeksforgeeks and tutorialspoint was sources.
-		
+		//TODO: Make our own test file... mitra asks for it?????
 	}
 
 }
---
+++ src/setbst/MKTest.java
@@ -0,0 +1,10 @@
+package setbst;
+
+public class MKTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
